c646b27949d686947c91e2fc678ec5a7c8941a0f
piusprince <45078705+piusprince@users.noreply.github.com>
2022-06-17 17:19:46 +0000

    Create typography versions

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+export const primaryFont = '"Roboto Mono", monospace';
+
+export const typeScale = {
+  header1: "3.90rem",
+  header2: "3.125rem",
+  header3: "2.5rem",
+  header4: "2rem",
+  header5: "1.6rem",
+  paragraph: "1.6rem",
+  helperText: "1.28rem",
+  copyrightText: "1.02rem",
+};
