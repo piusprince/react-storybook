29610ee4fb43d5f007e1f98a72c7de980bdb30e3
piusprince <45078705+piusprince@users.noreply.github.com>
2022-06-17 17:18:50 +0000

    Create colors utilities

---
+++ src/utils/colors.js
@@ -0,0 +1,33 @@
+export const blue = {
+  100: "#438ad8",
+  200: "#266ebb",
+  300: "#1d528c",
+  400: "#13375d",
+  500: "#a1c4eb",
+};
+
+export const neutral = {
+  100: "#ffffff",
+  200: "#bfbfbf",
+  300: "#7f7f7f",
+  400: "#3f3f3f",
+  500: "#000000",
+};
+
+export const green = {
+  100: "#8CDF99",
+  200: "#53CF66",
+  300: "#2FAB41",
+};
+
+export const red = {
+  100: "#FF2C1C",
+  200: "#E20F00",
+  300: "#AA0B00",
+};
+
+export const yellow = {
+  100: "#FEFE55",
+  200: "#FFFF1C",
+  300: "#E2E200                ",
+};
