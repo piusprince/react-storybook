f251d109a41c029db030ec740503a63207f7cabf
piusprince <45078705+piusprince@users.noreply.github.com>
2022-06-17 17:18:26 +0000

    Create global styling

---
+++ src/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+
+    }
+    html {
+        font-size: 62.5%;
+    }
+    body {
+        font-family: 'Roboto Mono',Roboto, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+`;
