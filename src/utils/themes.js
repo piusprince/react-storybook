7c4a4d0deedb67807d6b8a8d5d831d995700b42b
piusprince <45078705+piusprince@users.noreply.github.com>
2022-06-17 17:19:19 +0000

    Create themes

---
+++ src/utils/themes.js
@@ -0,0 +1,12 @@
+import { blue, neutral } from "./colors";
+import { primaryFont } from "./typography";
+
+export const defaultTheme = {
+  primaryColor: blue[100],
+  primaryColorHover: blue[300],
+  primaryColorActive: blue[100],
+  textColorOnPrimary: neutral[500],
+  textColor: neutral[100],
+  textColorInverted: neutral[500],
+  primaryFont,
+};
